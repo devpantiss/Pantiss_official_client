a513349a80dd81fb66658a4cb3795d45a626f789
devpantiss <dev.pantiss@gmail.com>
2025-01-16 18:39:02 +0530

    updates

--- src/Components/Summit/Banner.jsx
+++ src/Components/Summit/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
     <section className="relative bg-gray-50">
       {/* Background Image Section */}
       <div
-        className="relative w-full h-[600px] bg-cover flex justify-center items-center bg-center"
+        className="relative w-full h-[800px] bg-cover flex justify-center items-center bg-center"
         style={{
           backgroundImage:
             "url('https://res.cloudinary.com/dgtc2fvgu/image/upload/v1735198640/image-20161107-4688-19m41fk_epc1ml.avif')",
